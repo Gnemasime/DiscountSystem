49451aaa8eb2f181113fd7d8ee1060a0c14fb324
Gnemasime <158264023+Gnemasime@users.noreply.github.com>
2024-04-06 18:36:26 +0200

    Add files via upload

---
+++ Controllers/DiscountController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DiscountSystem.Controllers
+{
+    public class DiscountController : Controller
+    {
+        // GET: Discount
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Discount()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Discount(DiscountSystem.Models.Discount discount)
+        {
+            discount.Subtotal = discount.Sub();
+            discount.DiscRate = discount.Disc();
+            discount.TotalAmt = discount.Tot();
+            discount.Vat = discount.VatAmt();
+            discount.FinalDue = discount.Final();
+            return View(discount);
+        }
+    }
+}
